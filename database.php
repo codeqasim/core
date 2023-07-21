1dc070dd905c7fefa63b655197ebd9263a700f8d
Qasim Hussain <44278286+codeqasim@users.noreply.github.com>
2023-07-22 02:05:16 +0500

    i

---
+++ database.php
@@ -0,0 +1,19 @@
+<?php
+
+// return [
+//     'adapter' => [
+// 'driver' => 'Pdo_Mysql',
+//         'database' => 'khudcar_db',
+//         'username' => 'root',
+//         'password' => '',
+//         'charset' => 'utf8'
+//     ]
+// ];
+
+return [
+    'adapter' => [
+        'driver' => 'Pdo_Sqlite',
+        'database' => 'db/db.db',
+        'charset' => 'utf8'
+    ]
+];
--- index.php
+++ index.php
@@ -1,1 +1,28 @@
-<?php include "app/app.php"; ?>
+<?php
+
+include("vendor/autoload.php");
+
+use GroceryCrud\Core\GroceryCrud;
+
+$database = include('database.php');
+$config = include('config.php');
+
+$crud = new GroceryCrud($config, $database);
+
+$crud->setTable('users');
+// $crud->setSubject('Customer', 'Customers');
+//$crud->columns(['customerName', 'contactLastName', 'contactFirstName']);
+
+$output = $crud->render();
+
+if ($output->isJSONResponse) {
+    header('Content-Type: application/json; charset=utf-8');
+    echo $output->output;
+    exit;
+}
+
+$css_files = $output->css_files;
+$js_files = $output->js_files;
+$output = $output->output;
+
+include('view.php');
